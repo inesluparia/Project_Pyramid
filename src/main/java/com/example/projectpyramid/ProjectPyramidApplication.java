5635850d5e7543b0b8a46905b9d1d9ce0e6f6ba8
Ines L <inesluparia@gmail.com>
2021-05-06 09:40:52 +0200

    Initial commit

---
+++ src/main/java/com/example/projectpyramid/ProjectPyramidApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectpyramid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPyramidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPyramidApplication.class, args);
+    }
+
+}
