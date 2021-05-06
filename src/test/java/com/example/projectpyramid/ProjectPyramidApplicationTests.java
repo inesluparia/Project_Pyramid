696a89b43414a01881dfac11795035952d551bb2
Ines L <inesluparia@gmail.com>
2021-05-06 09:40:52 +0200

    Initial commit

---
+++ src/test/java/com/example/projectpyramid/ProjectPyramidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectpyramid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPyramidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
