91c7a5d3e3badd0a32c214542b1c1a5ea45235af
davidfrp <davidfrp@outlook.dk>
2021-05-11 10:25:26 +0200

    Switched from id references to object references.

--- src/main/java/com/example/projectpyramid/domain/entities/Project.java
+++ src/main/java/com/example/projectpyramid/domain/entities/Project.java
@@ -2,58 +2,65 @@ package com.example.projectpyramid.domain.entities;
 
 public class Project {
 
-    private int projectId, authorId, clientId;
-    //private Client client;
+    private int id;
+    private User author;
+    private Client client;
     private String projectName, description;
     private boolean isActive;
 
-    public Project(){
-    isActive = false;
+    public Project() {
+        isActive = false;
     }
 
-    public Project( int projectId,
-                    int authorId,
-                    int clientId,
-                    String projectName,
-                    String description){
-
-        this.projectId = projectId;
-        this.authorId = authorId;
-        this.clientId = clientId;
+    public Project(int id,
+                   User author,
+                   Client client,
+                   String projectName,
+                   String description) {
+        this.id = id;
+        this.author = author;
+        this.client = client;
         this.projectName = projectName;
         this.description =  description;
     }
 
     //Getters
-    public int getProjectId(){
 
-        return projectId;
+    public int getId() {
+        return this.id;
     }
 
-    public String getDescription(){
-
-        return this.description;
+    public User getAuthor() {
+        return this.author;
     }
 
-    public String getProjectName(){
+    public Client getClient() {
+        return this.client;
+    }
 
+    public String getProjectName() {
         return this.projectName;
     }
 
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean getIsActive() {
+        return this.isActive;
+    }
+
     //Setters
-    public void setActive(){
-        isActive = true;
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
     }
 
-    public void setDescription(String editDescription){
-
-        this.description = editDescription;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public void setProjectName(String newProjectName){
-
-        this.projectName = newProjectName;
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
     }
-
-
 }
